78a7d625c2ab68e4e9ebe666379333914a760163
KandhanSekar <kandhan3694@gmail.com>
2016-09-29 20:45:15 -0700

    nu

---
+++ q1.py
@@ -0,0 +1,15 @@
+# your code goes here
+import sys
+var=input()
+for i in range(1,6):
+	a=''
+	for j in range(0,i):
+		a=a+str(i)
+	print(a)
+	
+for k in range(1,6):
+	f=''
+	for h in range(1,7-k):
+		f=f+str(h)
+	
+	print(f)
